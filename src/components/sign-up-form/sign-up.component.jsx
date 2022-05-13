19d575343c5987fd28a343dfa6f5971b12ac016b
csmith <cody.charles.smith@gmail.com>
2022-05-13 11:19:48 -0400

    Section 4.100 - Adds customer Button component, adds styling for the sign-up component and button, adds Button to sign-up component

--- src/components/sign-up-form/sign-up.component.jsx
+++ src/components/sign-up-form/sign-up.component.jsx
@@ -1,6 +1,8 @@
 import {useState} from 'react'
 import {createAuthUserWithEmailAndPassword, createUserDocumentFromAuth} from '../../utils/firebase/firebase.utils'
 import FormInput from '../form-input/form-input.component'
+import './sign-up.styles.scss'
+import Button from "../button/button.component";
 
 const defaultFormFields = {
     displayName: '',
@@ -49,8 +51,9 @@ const SignUpForm = () => {
     }
 
     return (
-        <div>
-            <h1>Sign up with your email and password</h1>
+        <div className='sign-up-container'>
+            <h2>Don't have an account?</h2>
+            <span>Sign up with your email and password</span>
             <form onSubmit={handleSubmit}>
                 <FormInput
                     label='Display Name'
@@ -84,7 +87,7 @@ const SignUpForm = () => {
                     name='confirmPassword'
                     value={ confirmPassword }
                 />
-                <button type='submit'>Sign Up</button>
+                <Button type='submit'>Sign Up</Button>
             </form>
         </div>
     )
