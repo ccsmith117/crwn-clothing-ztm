48addb07c6a4477e4954465f342e8b644eb543dd
csmith <cody.charles.smith@gmail.com>
2022-05-21 15:32:28 -0400

    Section 11.139 - Refactors form.input to use styled-components

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,16 +1,17 @@
-import './form-input.styles.scss'
+import './form-input.styles'
+import {FormInputLabel, Group, Input} from './form-input.styles'
 
 const FormInput = ( {label, ...otherProps}) => {
     return (
-        <div className='group'>
-            <input className='form-input' {...otherProps}/>
-            <label
-                className={`${otherProps.value.length ? 'shrink' : ''} form-input-label`}
+        <Group>
+            <Input {...otherProps}/>
+            <FormInputLabel
+                shrink={otherProps.value.length}
             >
-                { label }
-            </label>
-        </div>
-)
+                {label}
+            </FormInputLabel>
+        </Group>
+    )
 }
 
 export default FormInput
