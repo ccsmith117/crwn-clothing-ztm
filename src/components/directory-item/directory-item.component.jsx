b338948700c5b2a4666da04f69eb0c5828f80ff5
csmith <cody.charles.smith@gmail.com>
2022-05-21 14:59:38 -0400

    Section 11.138 - Refactors directory-item.component to use styled-components

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,16 +1,17 @@
-import './directory-item.styles.scss'
+import './directory-item.styles'
+import {BackgroundImage, Body, DirectoryItemContainer} from './directory-item.styles'
 
 const DirectoryItem = ({ category }) => {
     const { title, imageUrl } = category
-    return <div className='directory-item-container'>
-        <div className='background-image' style={{
-            backgroundImage: `url(${imageUrl})`
-        }}/>
-        <div className='body'>
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-        </div>
-    </div>
+    return (
+        <DirectoryItemContainer>
+            <BackgroundImage imageUrl={imageUrl}/>
+            <Body>
+                <h2>{title}</h2>
+                <p>Shop Now</p>
+            </Body>
+        </DirectoryItemContainer>
+    )
 }
 
 export default DirectoryItem
