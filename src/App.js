1f3c2c4dd8e4cbb92b4e538da88b1ae0bd3bd68e
csmith <cody.charles.smith@gmail.com>
2022-05-30 20:42:22 -0400

    Section 14.153 - Converts use of user.context.jsx with user.reducer.js and helper functions

--- src/App.js
+++ src/App.js
@@ -5,8 +5,22 @@ import Navigation from './routes/navigation/navigation.component'
 import AuthenticationComponent from './routes/authentication/authentication.component'
 import Shop from './components/shop/shop.component'
 import Checkout from './components/checkout/checkout.component'
+import {useEffect} from 'react'
+import {createUserDocumentFromAuth, onAuthStateChangedListener} from './utils/firebase/firebase.utils'
+import {setCurrentUser} from './store/user/user.action'
+import {useDispatch} from 'react-redux'
 
 const App = () => {
+    const dispatch = useDispatch()
+    useEffect(() => {
+        return onAuthStateChangedListener((user) => {
+            if (user) {
+                createUserDocumentFromAuth(user)
+            }
+            dispatch(setCurrentUser(user))
+        })
+    }, [])
+
     return (
         <Routes>
             <Route path='/' element={<Navigation />}>
--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import {BrowserRouter} from 'react-router-dom'
 import './index.scss'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-import {UserProvider} from './contexts/user.context'
 import {CategoriesProvider} from './contexts/categories.context'
 import {CartProvider} from './contexts/cart.context'
 import {store} from './store/store'
@@ -16,13 +15,11 @@ root.render(
     <React.StrictMode>
         <Provider store={store}>
             <BrowserRouter>
-                <UserProvider>
-                    <CategoriesProvider>
-                        <CartProvider>
-                            <App />
-                        </CartProvider>
-                    </CategoriesProvider>
-                </UserProvider>
+                <CategoriesProvider>
+                    <CartProvider>
+                        <App />
+                    </CartProvider>
+                </CategoriesProvider>
             </BrowserRouter>
         </Provider>
 
