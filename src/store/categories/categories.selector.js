d9cac974d4c2cd1b1b8aa05e32a0e25a4d064259
csmith <cody.charles.smith@gmail.com>
2022-06-07 08:17:19 -0400

    Section 14.161 - Adds reselect library to add memoization to reducers and avoid re-rendering when unnecessary

--- src/store/categories/categories.selector.js
+++ src/store/categories/categories.selector.js
@@ -1,6 +1,20 @@
-export const selectCategoriesMap = (state) =>
-    state.categories.categories.reduce((accumulator, category) => {
-        const {title, items} = category
-        accumulator[title.toLowerCase()] = items
-        return accumulator
-    }, {})
+import {createSelector} from 'reselect'
+
+const selectCategoryReducer = (state) => state.categories
+
+export const selectCategories = createSelector(
+    [selectCategoryReducer],
+    (categoriesSlice) => categoriesSlice.categories
+)
+
+export const selectCategoriesMap = createSelector(
+    [selectCategories],
+    (categories) => {
+        console.log('selector fired')
+        return categories.reduce((accumulator, category) => {
+            const {title, items} = category
+            accumulator[title.toLowerCase()] = items
+            return accumulator
+        }, {})
+    }
+)
