ed4542370c0704cbcaf66424033aa00dc9726ec3
csmith <cody.charles.smith@gmail.com>
2022-10-07 08:22:06 -0400

    Section 5.85-87 - Adds initial navigation bar with subrouting and links in place to route to home component and shop component, adds root level styling for all elements and anchor elements

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import {Route, Routes} from 'react-router-dom'
+import NavigationBar from './routes/navigation-bar/navigation-bar.component'
 import Home from './routes/home/home.component'
+
+const Shop = () => {
+    return (<div>Shopping time</div>)
+}
 
 const App = () => {
   return (
       <Routes>
-        <Route path='/home' element={<Home />} />
-        <Route path='/' element={<div>Whoops- wrong spot.  Go to <a href={'/home'}>/home</a></div>} />
+          <Route path='/' element={<NavigationBar />}>
+              <Route path='' element={<Home />}/>
+              <Route path='shop' element={<Shop />}/>
+          </Route>
       </Routes>
   )
 }
