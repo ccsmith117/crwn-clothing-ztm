22ac37fecb8915538b8233828611f809e807ad83
csmith <cody.charles.smith@gmail.com>
2022-10-10 08:01:12 -0400

    Section 5.87 - Updates route component to use index instead of an empty path in order to show when at the parent route path

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
   return (
       <Routes>
           <Route path='/' element={<NavigationBar />}>
-              <Route path='' element={<Home />}/>
+              <Route index element={<Home />}/>
               <Route path='shop' element={<Shop />}/>
           </Route>
       </Routes>
