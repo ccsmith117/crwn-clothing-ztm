19b4d0f814da8bb9ad4f990a0f56565ec3873c0b
csmith <cody.charles.smith@gmail.com>
2022-12-02 08:44:28 -0500

    Section 9.112-113 - Adds context for Products to display on the Shop page

--- src/App.js
+++ src/App.js
@@ -2,10 +2,7 @@ import {Route, Routes} from 'react-router-dom'
 import NavigationBar from './routes/navigation-bar/navigation-bar.component'
 import Home from './routes/home/home.component'
 import Authentication from './routes/authentication/authentication.component'
-
-const Shop = () => {
-    return (<div>Shopping time</div>)
-}
+import Shop from './routes/shop/shop.component'
 
 const App = () => {
   return (
---
+++ src/routes/shop/shop.component.jsx
@@ -0,0 +1,21 @@
+import {useContext} from 'react'
+import {ProductsContext} from '../../context/products.context'
+
+const Shop = () => {
+    const {products} = useContext(ProductsContext)
+    console.log(products)
+
+    return (
+        <div>
+            {
+                products.map((product) =>
+                    <div key={product.id}>
+                        <h1>{product.name}</h1>
+                        <p>{product.price}</p>
+                    </div>)
+            }
+        </div>
+    )
+}
+
+export default Shop
