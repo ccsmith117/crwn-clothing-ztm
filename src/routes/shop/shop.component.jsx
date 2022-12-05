d34bf7a8e204a3d25620137365deb1c4e176cba8
csmith <cody.charles.smith@gmail.com>
2022-12-05 08:23:18 -0500

    Section 9.114 - Adds products to Shop page and breaks up components into individual components

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,19 +1,15 @@
 import {useContext} from 'react'
 import {ProductsContext} from '../../context/products.context'
+import Product from '../../components/product/product.component'
+import './shop.styles.scss'
 
 const Shop = () => {
     const {products} = useContext(ProductsContext)
     console.log(products)
 
     return (
-        <div>
-            {
-                products.map((product) =>
-                    <div key={product.id}>
-                        <h1>{product.name}</h1>
-                        <p>{product.price}</p>
-                    </div>)
-            }
+        <div className='products-container'>
+            { products.map((product) => <Product key={product.id} product={product} />) }
         </div>
     )
 }
