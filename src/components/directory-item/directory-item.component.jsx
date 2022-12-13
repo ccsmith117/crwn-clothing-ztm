49fef3a2a2fb058ba26ca6184dbbef37275ddbd9
csmith <cody.charles.smith@gmail.com>
2022-12-13 08:21:20 -0500

    Section 11.135 - Wires directory items to product subpages

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,10 +1,11 @@
 import './directory-item.styles.scss'
+import {Link} from 'react-router-dom'
 
 const DirectoryItem = (props) => {
     const {category} = props
     const {imageUrl, title} = category
     return (
-        <div className="directory-item-container">
+        <Link className='directory-item-container' to={`shop/${title.toLowerCase()}`}>
             <div className='background-image' style={{
                 backgroundImage: `url(${imageUrl})`
             }} />
@@ -12,7 +13,7 @@ const DirectoryItem = (props) => {
                 <h2>{title}</h2>
                 <p>Shop Now</p>
             </div>
-        </div>
+        </Link>
     )
 }
 
