6f152704685a1b1a1b5b73da3dd1449b0eb86ca3
csmith <cody.charles.smith@gmail.com>
2022-12-13 08:36:46 -0500

    Section 11.136 - Converts cart-icon to using styled-components, tweaks styling for checkout-item to disable user select on the remove "button"

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,17 +1,16 @@
-import './cart-icon.styles.scss'
-import {ReactComponent as ShoppingBag} from '../../assets/shopping-bag.svg'
 import {useContext} from 'react'
 import {CartContext} from '../../context/cart.context'
+import {CartIconContainer, ItemCount, ShoppingIcon} from './cart-icon.styles'
 
 const CartIcon = () => {
     const {isCartOpen, setIsCartOpen, cartItemCount} = useContext(CartContext)
     const toggleCartVisibility = () => setIsCartOpen(!isCartOpen)
 
     return (
-        <div className='cart-icon-container' onClick={toggleCartVisibility}>
-            <ShoppingBag className='shopping-icon' />
-            <span className='item-count'>{cartItemCount}</span>
-        </div>
+        <CartIconContainer onClick={toggleCartVisibility}>
+            <ShoppingIcon />
+            <ItemCount>{cartItemCount}</ItemCount>
+        </CartIconContainer>
     )
 }
 
