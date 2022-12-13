f5b184ec237857c5f4e081301a6655653abb75ad
csmith <cody.charles.smith@gmail.com>
2022-12-13 08:30:59 -0500

    Section 11.136 - Converts cart-dropdown to using styled-components

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,9 +1,9 @@
-import './cart-dropdown.styles.scss'
 import Button from '../button/button.component'
 import CartItem from '../cart-item/cart-item.component'
 import {useContext} from 'react'
 import {CartContext} from '../../context/cart.context'
 import {useNavigate} from 'react-router-dom'
+import {CartDropdownContainer, CartItemsContainer, EmptyMessage} from './cart-dropdown.styles'
 
 const CartDropdown = () => {
     const {cartItems, setIsCartOpen} = useContext(CartContext)
@@ -14,17 +14,27 @@ const CartDropdown = () => {
         setIsCartOpen(false)
     }
 
-    return <div className='cart-dropdown-container'>
-        <div className='cart-items'>
-            {
-                cartItems.length > 0 ?
-                    cartItems.map((cartItem) => <CartItem key={cartItem.id} cartItem={cartItem}/>)
-                    :
-                    <span className='empty-message'>Your cart is empty :(</span>
-            }
-        </div>
-        <Button onClick={gotToCheckout}>CHECKOUT</Button>
-    </div>
+    const CartItems = () => {
+        return (
+            <>
+                {
+                    cartItems.length > 0 ?
+                        cartItems.map((cartItem) => <CartItem key={cartItem.id} cartItem={cartItem}/>)
+                        :
+                        <EmptyMessage>Your cart is empty :(</EmptyMessage>
+                }
+            </>
+        )
+    }
+
+    return (
+        <CartDropdownContainer>
+            <CartItemsContainer>
+                <CartItems />
+            </CartItemsContainer>
+            <Button onClick={gotToCheckout}>CHECKOUT</Button>
+        </CartDropdownContainer>
+    )
 }
 
 export default CartDropdown
