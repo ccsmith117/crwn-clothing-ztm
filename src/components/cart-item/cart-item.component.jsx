1c7bcc5b20185a08a86ed4ef24afc6b04782d85f
csmith <cody.charles.smith@gmail.com>
2022-12-14 08:41:44 -0500

    Section 11.136 - Converts cart-item to using styled-components

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,15 +1,17 @@
-import './cart-item.styles.scss'
+import './cart-item.styles'
+import {CartItemContainer, ItemDetails, ItemImage, ItemName} from './cart-item.styles'
 
 const CartItem = ({cartItem}) => {
     const {imageUrl, name, price, quantity} = cartItem
     return (
-        <div className='cart-item-container'>
-            <img src={imageUrl} alt={`${name}`} />
-            <div className='item-details'>
-                <span className='name'>{name}</span>
+        <CartItemContainer>
+            <ItemImage src={imageUrl} alt={`${name}`} />
+            <ItemDetails>
+                <ItemName>{name}</ItemName>
                 <span>{`${quantity} x $${price}`}</span>
-            </div>
-        </div>)
+            </ItemDetails>
+        </CartItemContainer>
+    )
 }
 
 export default CartItem
