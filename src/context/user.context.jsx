58d6299b9238974f81207a8516780fee678017e5
csmith <cody.charles.smith@gmail.com>
2022-12-20 09:27:21 -0500

    Section 13.145 - Updates user context with reducer pattern

--- src/context/user.context.jsx
+++ src/context/user.context.jsx
@@ -1,4 +1,4 @@
-import {createContext, useEffect, useState} from 'react'
+import {createContext, useEffect, useReducer} from 'react'
 import {createUserDocumentFromAuth, onAuthStateChangeListener} from '../utils/firebase/firebase.utils'
 
 export const UserContext = createContext({
@@ -6,16 +6,42 @@ export const UserContext = createContext({
     setCurrentUser: () => null
 })
 
+export const USER_ACTION_TYPES = {
+    SET_CURRENT_USER: 'SET_CURRENT_USER'
+}
+
+const userReducer = (state, action) => {
+    const {type, payload} = action
+
+    switch (type) {
+        case 'SET_CURRENT_USER':
+            return {
+                ...state,
+                currentUser: payload
+            }
+        default:
+            throw new Error(`Unhandled type ${type} in userReducer`)
+    }
+}
+
+const INITIAL_STATE = {
+    currentUser: null
+}
+
 export const UserProvider = ({children}) => {
-    const [currentUser, setCurrentUser] = useState(null)
+    const [{currentUser}, dispatch] = useReducer(userReducer, INITIAL_STATE)
+
+    const setCurrentUser = (user) => {
+        dispatch({type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user})
+    }
+
     const value = { currentUser, setCurrentUser}
 
     useEffect(() => {
-        const unsubscribe = onAuthStateChangeListener(async (user) => {
+        return onAuthStateChangeListener(async (user) => {
             await createUserDocumentFromAuth(user)
             setCurrentUser(user)
         })
-        return unsubscribe
     }, [])
 
     return <UserContext.Provider value={value}>{children}</UserContext.Provider>
