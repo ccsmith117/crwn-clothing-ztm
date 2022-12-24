24889f89b6d4a4d7b3880e223c231fb82e5d54ab
csmith <cody.charles.smith@gmail.com>
2022-12-24 11:35:51 -0600

    Section 14.151-154 - Refactors user.context and context api to utilizing redux

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,20 @@ import Home from './routes/home/home.component'
 import Authentication from './routes/authentication/authentication.component'
 import Shop from './routes/shop/shop.component'
 import Checkout from './routes/checkout/checkout.component'
+import {setCurrentUser} from './store/user/user.action'
+import {useEffect} from 'react'
+import {createUserDocumentFromAuth, onAuthStateChangeListener} from './utils/firebase/firebase.utils'
+import {useDispatch} from 'react-redux'
 
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+      return onAuthStateChangeListener(async (user) => {
+          await createUserDocumentFromAuth(user)
+          dispatch(setCurrentUser(user))
+      })
+  }, [])
+
   return (
       <Routes>
           <Route path='/' element={<NavigationBar />}>
--- src/routes/navigation-bar/navigation-bar.component.jsx
+++ src/routes/navigation-bar/navigation-bar.component.jsx
@@ -1,15 +1,16 @@
 import {Outlet} from 'react-router-dom'
 import {ReactComponent as CrwnLogo} from '../../assets/crown.svg'
 import {useContext} from 'react'
-import {UserContext} from '../../context/user.context'
 import {signOutUser} from '../../utils/firebase/firebase.utils'
 import CartIcon from '../../components/cart-icon/cart-icon.component'
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component'
 import {CartContext} from '../../context/cart.context'
 import {LogoContainer, NavigationBarContainer, NavigationLink, NavigationLinksContainer} from './navigation-bar.styles'
+import {useSelector} from 'react-redux'
+import {selectCurrentUser} from '../../store/user/user.selector'
 
 const NavigationBar = () => {
-    const {currentUser} = useContext(UserContext)
+    const currentUser = useSelector(selectCurrentUser)
     const {isCartOpen} = useContext(CartContext)
 
     return (
