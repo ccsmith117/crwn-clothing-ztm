c9f05d90afe3642190c6c4af6c5976e337dc420d
csmith <cody.charles.smith@gmail.com>
2022-12-30 10:03:04 -0500

    Section 14.162 - Adds reselect library to enable selector memoization and uses it to optimize our categories selectors to not calculate the categories map unnecessarily

--- src/store/categories/categories.selector.js
+++ src/store/categories/categories.selector.js
@@ -1,5 +1,12 @@
-export const selectCategoriesMap = (state) => state.categories.categories.reduce((accumulator, category) => {
-    const {title, items} = category
-    accumulator[title.toLowerCase()] = items
-    return accumulator
-}, {})
+import {createSelector} from 'reselect'
+
+const selectCategoryState = (state) => state.categories
+
+const selectCategories = createSelector([selectCategoryState], (categories) => categories.categories)
+
+export const selectCategoriesMap = createSelector([selectCategories],(categoriesSlice) => categoriesSlice.reduce((accumulator, category) => {
+        const {title, items} = category
+        accumulator[title.toLowerCase()] = items
+        return accumulator
+    }, {})
+)
