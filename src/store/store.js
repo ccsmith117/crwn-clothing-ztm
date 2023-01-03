731f2f48f90886c5157b9f8fdc0295e290fcf903
csmith <cody.charles.smith@gmail.com>
2023-01-02 19:57:30 -0500

    Section 15.167 - Adds redux-persist to application to enable storing our redux state into local storage to rehydrate the state post refresh

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,22 @@
 import {compose, createStore, applyMiddleware} from 'redux'
 import logger from 'redux-logger'
-
 import {rootReducer} from './root-reducer'
+import {persistReducer, persistStore} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 const middleWares = [logger]
 
 const composeEnhancers = compose(applyMiddleware(...middleWares))
 
+const persistConfig = {
+    key: 'root',
+    storage,
+    blacklist: ['user']
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 // todo: update deprecated createStore method to use redux-toolkit
-export const store = createStore(rootReducer, undefined, composeEnhancers)
+export const store = createStore(persistedReducer, undefined, composeEnhancers)
+
+export const persistor = persistStore(store)
