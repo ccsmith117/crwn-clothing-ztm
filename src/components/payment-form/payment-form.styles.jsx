45299c7d90ca64495dba2e5e3a1c2d244596f6d9
csmith <cody.charles.smith@gmail.com>
2023-01-12 08:54:25 -0500

    Section 18.186 - Temp fixes bug with stripe component not rendering

--- src/components/payment-form/payment-form.styles.jsx
+++ src/components/payment-form/payment-form.styles.jsx
@@ -1,14 +1,6 @@
 import styled from 'styled-components'
 
-export const PaymentFormContainer = styled.div`
-  height: 300px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-`
-
-export const FormContainer = styled.div`
+export const FormContainer = styled.form`
   height: 100px;
   width: 500px;
 `
