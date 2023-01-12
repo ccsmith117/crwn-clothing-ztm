f6c7e73985efd8e78103521d25d8f0e170cb3344
csmith <cody.charles.smith@gmail.com>
2023-01-12 09:26:34 -0500

    Section 18.186 - Completes payment intent with adding logic to confirm card details and process with stripe's API

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -22,8 +22,21 @@ const PaymentForm = () => {
             body: JSON.stringify({amount: 10000})
         }).then(res => res.json())
 
-        console.log(response)
+        const clientSecret = response.paymentIntent.client_secret
+        const paymentResult = await stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: elements.getElement(CardElement),
+                billing_details: {
+                    name: 'Bob Bobber'
+                }
+            }
+        })
 
+        if (paymentResult.error) {
+            alert(paymentResult.error)
+        } else if (paymentResult.paymentIntent.status === 'succeeded') {
+            alert('Payment Successful')
+        }
     }
 
     return (
